f87bfd865886f8acf6108fc28ce68a50e42fc4f7
goldmojo <goldmojo@free.fr>
2020-05-13 19:01:09 +0200

    Activate alsa instead of pulse audio

--- build_rg350.sh
+++ build_rg350.sh
@@ -1,10 +1,6 @@
-./configure  CC="/opt/gcw0-toolchain/usr/bin/mipsel-gcw0-linux-uclibc-gcc" CFLAGS="-D_POSIX_C_SOURCE" CFLAGS_BUILD="-D_POSIX_C_SOURCE"
+./configure --with-pulse="0" CC="/opt/gcw0-toolchain/usr/bin/mipsel-gcw0-linux-uclibc-gcc" CFLAGS="-D_POSIX_C_SOURCE" CFLAGS_BUILD="-D_POSIX_C_SOURCE"
 
 make all
-
 cp objs/release/opendune opk
-cp /opt/gcw0-toolchain/usr/mipsel-gcw0-linux-uclibc/sysroot/usr/lib/libpulse.so.0 opk
-cp /opt/gcw0-toolchain/usr/mipsel-gcw0-linux-uclibc/sysroot/usr/lib/libjson-c.so.2 opk
-cp /opt/gcw0-toolchain/usr/mipsel-gcw0-linux-uclibc/sysroot/usr/lib/pulseaudio/libpulsecommon-5.0.so opk
+rm -f opendune.opk
 mksquashfs opk opendune.opk
-
